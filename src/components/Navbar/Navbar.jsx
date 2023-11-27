276487aa0c5993767d7095b11bc97de1a332c7f9
valenty-nka <valenty.nechyporenko@gmail.com>
2023-11-26 20:17:59 -0400

    Add images and icons to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
+import './Navbar.css';
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
