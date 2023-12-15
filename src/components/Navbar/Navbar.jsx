8e3d1d5091303501434c1885c1e39ba89ec82459
valenty-nka <valenty.nechyporenko@gmail.com>
2023-12-15 01:52:50 -0400

    Add logo and navbar. Change the style of the navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-//import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import './Navbar.css';
 import images from '../../constants/images';
@@ -8,7 +8,19 @@ import './Navbar.css';
 
 const Navbar = () => (
   <nav className='app__navbar'>
-    Navbar
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt='app logo' />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href='#home'>Home</a></li>
+      <li className='p__opensans'><a href='#about'>About</a></li>
+      <li className='p__opensans'><a href='#menu'>Menu</a></li>
+      <li className='p__opensans'><a href='#awards'>Awards</a></li>
+      <li className='p__opensans'><a href='#contact'>Contant</a></li>
+    </ul>
+    <div className='app__navbar-menu'>
+      <GiHamburgerMenu />
+    </div>
   </nav>
 );
 
