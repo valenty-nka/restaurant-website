2287db9225ff6f2a47ec15f4d77e3cd32e80930b
valenty-nka <valenty.nechyporenko@gmail.com>
2024-02-03 23:21:27 -0400

    Implement the Navbar component and its styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,24 +6,44 @@ import './Navbar.css';
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.gericht} alt='app logo' />
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href='#home'>Home</a></li>
-      <li className='p__opensans'><a href='#about'>About</a></li>
-      <li className='p__opensans'><a href='#menu'>Menu</a></li>
-      <li className='p__opensans'><a href='#awards'>Awards</a></li>
-      <li className='p__opensans'><a href='#contact'>Contant</a></li>
-    </ul>
-    <div className='app__navbar-login'>
-      <a href="#login" target="_blank" rel="noopener noreferrer" className='p__opensans'>Log In / Register</a>
-      <div />
-      <a href='/' className='p__opensans'>Book a table</a>
-    </div>
-  </nav >
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt='app logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contant</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href="#login" target="_blank" rel="noopener noreferrer" className='p__opensans'>Log In / Register</a>
+        <div />
+        <a href='/' className='p__opensans'>Book a table</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu className='overlay__close' fontSize={27} onClick={() => setToggleMenu(false)} />
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              <li className='p__opensans'><a href='#contact'>Contant</a></li>
+            </ul>
+          </div>
+        )}
+
+      </div>
+    </nav >
+  )
+};
 
 export default Navbar;
