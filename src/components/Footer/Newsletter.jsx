c9f45e8250472d23dfd7b79e06cb192d8595d3f0
valenty-nka <valenty.nechyporenko@gmail.com>
2024-02-15 01:07:00 -0400

    Create Footer and its components

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import './Newsletter.css';
+import Subheading from '../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <Subheading title='Newsletter' />
+      <h1 className='headtext__cormorant'>Subscribe to Our Newsletter</h1>
+      <p className='p__opensans'>And never miss latest Updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your email' />
+      <button className='custom__button'>Subscribe</button>
+    </div>
   </div>
 );
 
